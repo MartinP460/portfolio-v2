d452b23a42d58071556530146702c458a17a9c9f
Martin Polley <martin.polley460@gmail.com>
2025-01-03 17:49:56 +0100

    Add background gradient.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,8 +5,12 @@
   let { data, children } = $props()
 </script>
 
-{#key data.url}
-  <div in:fly={{ x: -200, duration: 300, delay: 300 }} out:fly={{ x: 200, duration: 300 }}>
-    {@render children()}
-  </div>
-{/key}
+<div
+  class="from-background-gradient-start to-background-gradient-end h-screen w-screen bg-gradient-to-br"
+>
+  {#key data.url}
+    <div in:fly={{ x: -200, duration: 300, delay: 300 }} out:fly={{ x: 200, duration: 300 }}>
+      {@render children()}
+    </div>
+  {/key}
+</div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,22 @@
-import typography from '@tailwindcss/typography';
-import type { Config } from 'tailwindcss';
+import typography from '@tailwindcss/typography'
+import colors from 'tailwindcss/colors'
+import type { Config } from 'tailwindcss'
 
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
 
-	theme: {
-		extend: {}
-	},
+  theme: {
+    extend: {
+      colors: {
+        primary: '#283f13',
+        background: '#e0d1b8',
+        'background-gradient-start': '#e0d1b8',
+        'background-gradient-end': '#ecd4ad',
+        'text-foreground': colors.gray[800],
+        'text-background': colors.gray[600]
+      }
+    }
+  },
 
-	plugins: [typography]
-} satisfies Config;
+  plugins: [typography]
+} satisfies Config
