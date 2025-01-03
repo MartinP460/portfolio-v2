e1b58b3fb04960492a80a3fd4bce2943dbbb2303
Martin Polley <martin.polley460@gmail.com>
2025-01-03 19:10:51 +0100

    Add initial navbar.

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  const routes = [
+    {
+      name: 'projects',
+      href: '/projects',
+      order: 1
+    },
+    {
+      name: 'experience',
+      href: '/experience',
+      order: 2
+    },
+    {
+      name: 'education',
+      href: '/education',
+      order: 3
+    },
+    {
+      name: 'skills',
+      href: '/skills',
+      order: 4
+    },
+    {
+      name: 'about me',
+      href: '/about',
+      order: 5
+    }
+  ]
+</script>
+
+<header class="flex grid-flow-dense items-center justify-center p-8">
+  <nav>
+    <ul class="grid grid-cols-5 gap-x-12 text-lg text-gray-600">
+      {#each routes as { name, href }}
+        <li class="flex flex-col items-center">
+          <a {href}>
+            {name}
+          </a>
+        </li>
+      {/each}
+      <div class="order-last flex items-center justify-center">
+        <div class="mt-2 h-2 w-2 rounded-full bg-amber-600"></div>
+      </div>
+    </ul>
+  </nav>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,15 @@
   import '../app.css'
   import { fly } from 'svelte/transition'
   import Background from '$lib/components/background.svelte'
+  import Navbar from '$lib/components/navbar.svelte'
 
   let { data, children } = $props()
 </script>
 
 <div>
   <Background />
-  <main>
+  <Navbar />
+  <main class="container mx-auto max-w-3xl">
     {#key data.url}
       <div in:fly={{ x: -200, duration: 300, delay: 300 }} out:fly={{ x: 200, duration: 300 }}>
         {@render children()}
