eb7e9890782590ced4ead2a0240cbfbe5e001d3e
Martin Polley <martin.polley460@gmail.com>
2025-01-03 18:15:24 +0100

    Create background corners.

---
+++ src/lib/components/background.svelte
@@ -0,0 +1,18 @@
+<div
+  class="from-background-gradient-start to-background-gradient-end fixed -z-20 h-screen w-screen bg-gradient-to-br p-4"
+>
+  <div class="relative h-full w-full">
+    <div
+      class="absolute left-0 top-4 h-[1px] w-8 -rotate-[40deg] rounded-lg bg-gray-500 sm:top-6 sm:w-12"
+    ></div>
+    <div
+      class="absolute right-0 top-4 h-[1px] w-8 rotate-[40deg] rounded-lg bg-gray-500 sm:top-6 sm:w-12"
+    ></div>
+    <div
+      class="absolute bottom-4 left-0 h-[1px] w-8 rotate-[40deg] rounded-lg bg-gray-500 sm:bottom-6 sm:w-12"
+    ></div>
+    <div
+      class="absolute bottom-4 right-0 h-[1px] w-8 -rotate-[40deg] rounded-lg bg-gray-500 sm:bottom-6 sm:w-12"
+    ></div>
+  </div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
   import '../app.css'
   import { fly } from 'svelte/transition'
+  import Background from '$lib/components/background.svelte'
 
   let { data, children } = $props()
 </script>
 
-<div
-  class="from-background-gradient-start to-background-gradient-end h-screen w-screen bg-gradient-to-br"
->
-  {#key data.url}
-    <div in:fly={{ x: -200, duration: 300, delay: 300 }} out:fly={{ x: 200, duration: 300 }}>
-      {@render children()}
-    </div>
-  {/key}
+<div>
+  <Background />
+  <main>
+    {#key data.url}
+      <div in:fly={{ x: -200, duration: 300, delay: 300 }} out:fly={{ x: 200, duration: 300 }}>
+        {@render children()}
+      </div>
+    {/key}
+  </main>
 </div>
