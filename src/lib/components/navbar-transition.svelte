546a6a86f6f1477b29034cce8a046cfa8c1eab14
Martin Polley <martin.polley460@gmail.com>
2025-01-12 15:02:51 +0100

    More constant transitions.

--- src/lib/components/navbar-transition.svelte
+++ src/lib/components/navbar-transition.svelte
@@ -23,8 +23,8 @@
 </script>
 
 <div
-  in:fly={{ y: 100, duration: 200, delay: 250, easing: quadOut }}
-  out:fly={{ y: -100, duration: 200, easing: quadIn }}
+  in:fly={{ y: 100, duration: TRANSITION_DURATION, delay: TRANSITION_DELAY, easing: quadOut }}
+  out:fly={{ y: -100, duration: TRANSITION_DURATION, easing: quadIn }}
 >
   {#key url}
     <div
