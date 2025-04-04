a97b5d7d601b440c4b5560732a01a185be281c2c
Martin Polley <martin.polley460@gmail.com>
2025-04-04 17:20:20 +0200

    Add logo animation.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,43 @@
-<script>
-  import Socials from '$lib/components/socials.svelte'
+<script lang="ts">
+  import { navigating } from '$app/state'
   import { createPageTitle } from '$lib/utils/createPageTitle'
   import { navbarRoutesArray } from '$lib/utils/navbarRoutes'
+  import Socials from '$lib/components/socials.svelte'
+
+  let innerWidth = $state<number | null>(null)
+  let isMobile = $derived((innerWidth ?? 1025) < 1024)
+
+  let hasLoaded = $state(Boolean(navigating.from))
+
+  const shouldAnimate = $derived(!hasLoaded && !isMobile)
 </script>
 
 <svelte:head>
   <title>{createPageTitle('Portfolio')}</title>
 </svelte:head>
 
+<svelte:window bind:innerWidth />
+
+<span class={shouldAnimate ? 'animate-logo' : 'hidden'}>
+  <span>m</span>
+  <span>pol</span>
+</span>
 <div
-  class="mx-auto flex max-w-screen-lg flex-col justify-between px-6 font-body lg:h-4/6 lg:items-center lg:justify-center"
+  class="mx-auto flex max-w-screen-lg flex-col justify-between px-6 font-body lg:h-4/6 lg:items-center lg:justify-center {shouldAnimate &&
+    'animate-logo-bg'}"
 >
-  <div class="animate-fade-in-top lg:text-center">
+  <div class="lg:text-center">
     <p class="text-gray-600">Hey, I'm</p>
-    <h1 class="mt-2 font-title text-4xl font-bold text-primary lg:text-5xl">Martin Polley</h1>
+    <h1 class="mt-2 font-title text-4xl font-bold text-primary lg:text-5xl">martin polley</h1>
     <h2 class="mt-2 text-xl">Full-stack developer</h2>
   </div>
 
-  <div
-    class="animate-fade-in-top animate-delay-2 mt-10 flex flex-col gap-8 text-3xl lg:mt-20 lg:flex-row lg:gap-14 lg:text-lg"
-  >
+  <div class="mt-10 flex flex-col gap-8 text-3xl lg:mt-20 lg:flex-row lg:gap-14 lg:text-lg">
     {#each navbarRoutesArray as { name, href }}
       <a {href} class="font-body text-gray-700 transition-transform ease-in lg:hover:scale-105">
         {name}
       </a>
     {/each}
   </div>
-  <Socials class="animate-fade-in-top animate-delay-4 mt-16" />
+  <Socials class="mt-16" />
 </div>
