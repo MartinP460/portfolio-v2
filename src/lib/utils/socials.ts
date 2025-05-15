549f3785e49d92a3150d455e6a29dd565b33db6a
Martin Polley <martin.polley460@gmail.com>
2025-05-15 18:09:45 +0200

    Update socials.

--- src/lib/utils/socials.ts
+++ src/lib/utils/socials.ts
@@ -1,8 +1,8 @@
 export const GITHUB_URL = 'https://github.com/MartinP460'
 export const GITHUB_USERNAME = 'MartinP460'
 
-export const LINKEDIN_URL = 'https://www.linkedin.com/in/martin-polley-37b907247/'
+export const LINKEDIN_URL = 'https://www.linkedin.com/in/martpol/'
 
 export const RESUME_PATH = '/pdfs/resume.pdf'
 
-export const EMAIL_ADDRESS = 'martin.polley460@gmail.com'
+export const EMAIL_ADDRESS = 'mail@mpol.dev'
